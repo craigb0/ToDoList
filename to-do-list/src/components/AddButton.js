4767dfc84b4beb6ae93fdeec6df3c74bb819ae0e
craigb0 <33334233+craigb0@users.noreply.github.com>
2023-05-16 13:08:25 -0400

    Pretty formatting

--- to-do-list/src/components/AddButton.js
+++ to-do-list/src/components/AddButton.js
@@ -1,37 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddButton = (props) => {
-	const [taskData, setTaskData] = useState('');
-    const [done, setDone] = useState(false);
+  const [taskData, setTaskData] = useState("");
+  const [done, setDone] = useState(false);
 
-    const handleClick = () => {
-        if (taskData.trim() !== '') {
-          props.onClickFunction({ title: taskData, completed: done, id:props.id});
-          setTaskData('');
-          
-        }
-      };
-    
-    
-    return (
-        <div className='AddButton'>
-          Task Name:{' '}
-          <input
-            type="text"
-            value={taskData}
-            onChange={(e) => setTaskData(e.target.value)}
-          />
-          <br></br>
-          Done:{' '}
-          <input 
-            type="checkbox"  //won't uncheck for some reason
-            value={done}
-            onChange={(e) => setDone(e.target.checked)}
-          />
-          <br></br>
-          <button onClick={handleClick}>Add Task</button>
-        </div>
-      );
-}
+  const handleClick = () => {
+    if (taskData.trim() !== "") {
+      props.onClickFunction({ title: taskData, completed: done, id: props.id });
+      setTaskData("");
+    }
+  };
+
+  return (
+    <div className="AddButton">
+      Task Name:{" "}
+      <input
+        type="text"
+        value={taskData}
+        onChange={(e) => setTaskData(e.target.value)}
+      />
+      <br></br>
+      Done:{" "}
+      <input
+        type="checkbox" //won't uncheck for some reason
+        value={done}
+        onChange={(e) => setDone(e.target.checked)}
+      />
+      <br></br>
+      <button onClick={handleClick}>Add Task</button>
+    </div>
+  );
+};
 
 export default AddButton;
