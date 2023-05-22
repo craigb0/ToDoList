f19958b1351e23469ac2fb3b0281db8b29edafbf
craigb0 <33334233+craigb0@users.noreply.github.com>
2023-05-22 17:16:42 -0400

    Started Remind Button

--- to-do-list/src/components/RemindButton.js
+++ to-do-list/src/components/RemindButton.js
@@ -1,16 +1,68 @@
-import {DatePicker} from "@mui/x-date-pickers";
-import {MDBBtn, MDBIcon} from "mdb-react-ui-kit";
+import {renderTimeViewClock, DateTimePicker} from "@mui/x-date-pickers";
+import {
+	MDBBtn,
+	MDBIcon,
+	MDBModal,
+	MDBModalBody,
+	MDBModalContent,
+	MDBModalDialog,
+	MDBModalFooter,
+	MDBModalHeader,
+	MDBModalTitle,
+} from "mdb-react-ui-kit";
+import {useState} from "react";
+import * as dayjs from "dayjs";
 
-const RemindButton = (props) => {
-	return (
-		<MDBBtn
-			color='tertiary'
-			floating
-			// onClick={toggleModal}
-		>
-			<MDBIcon icon='clock' />
-		</MDBBtn>
-	);
+const RemindButton = () => {
+	const [modalVis, setModalVis] = useState(false);
+	const [time, setTime] = useState(dayjs());
+	var relativeTime = require("dayjs/plugin/relativeTime");
+	const saveTime = () => {};
+	const [running, setRunning] = useState(false);
+
+	const toggleModal = () => {
+		setModalVis(!modalVis);
+	};
+
+	if (!running) {
+		return (
+			<>
+				<MDBBtn color='tertiary' floating onClick={toggleModal}>
+					<MDBIcon icon='clock' />
+				</MDBBtn>
+				<MDBModal show={modalVis} setShow={setModalVis} tabIndex='-1'>
+					<MDBModalDialog>
+						<MDBModalContent>
+							<MDBModalHeader>
+								<MDBModalTitle>
+									Set Reminder{time.minute()}
+								</MDBModalTitle>
+								<MDBBtn
+									className='btn-close'
+									color='none'
+									onClick={toggleModal}
+								></MDBBtn>
+							</MDBModalHeader>
+							<MDBModalBody>
+								<DateTimePicker
+									disablePast={true}
+									value={time}
+									onChange={(e) => setTime(e)}
+								/>
+							</MDBModalBody>
+
+							<MDBModalFooter>
+								<MDBBtn color='secondary' onClick={toggleModal}>
+									Close
+								</MDBBtn>
+								<MDBBtn>Save changes</MDBBtn>
+							</MDBModalFooter>
+						</MDBModalContent>
+					</MDBModalDialog>
+				</MDBModal>
+			</>
+		);
+	}
 };
 
 export default RemindButton;
