8daf5432fb9ac287e43da4cd12f15ec2f4ddf199
craigb0 <33334233+craigb0@users.noreply.github.com>
2023-05-23 14:37:52 -0400

    Timer Implemented

--- to-do-list/src/components/RemindButton.js
+++ to-do-list/src/components/RemindButton.js
@@ -10,19 +10,48 @@ import {
 	MDBModalHeader,
 	MDBModalTitle,
 } from "mdb-react-ui-kit";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import * as dayjs from "dayjs";
 
 const RemindButton = () => {
 	const [modalVis, setModalVis] = useState(false);
-	const [time, setTime] = useState(dayjs());
-	var relativeTime = require("dayjs/plugin/relativeTime");
-	const saveTime = () => {};
+	const [time, setTime] = useState(dayjs().add(10, "minutes"));
 	const [running, setRunning] = useState(false);
+	const [secs, setSecs] = useState(0);
 
-	const toggleModal = () => {
+	const saveTime = () => {
+		setRunning(true);
+		setSecs(time.diff(dayjs(), "seconds"));
+		console.log(time);
+		toggleModal(true);
+	};
+
+	const toggleModal = (noReset = false) => {
+		if (noReset) {
+			setTime(dayjs().add(10, "minutes"));
+		}
 		setModalVis(!modalVis);
 	};
+
+	useEffect(() => {
+		let timer;
+		if (running) {
+			timer = setInterval(() => {
+				setSecs((prevSecs) => {
+					if (prevSecs <= 0) {
+						clearInterval(timer);
+						setRunning(false);
+						return 0;
+					}
+					return prevSecs - 1;
+				});
+			}, 1000);
+		}
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, [running, time]);
 
 	if (!running) {
 		return (
@@ -35,7 +64,7 @@ const RemindButton = () => {
 						<MDBModalContent>
 							<MDBModalHeader>
 								<MDBModalTitle>
-									Set Reminder{time.minute()}
+									Set Reminder{time.format("DD:HH:mm")}
 								</MDBModalTitle>
 								<MDBBtn
 									className='btn-close'
@@ -55,7 +84,39 @@ const RemindButton = () => {
 								<MDBBtn color='secondary' onClick={toggleModal}>
 									Close
 								</MDBBtn>
-								<MDBBtn>Save changes</MDBBtn>
+								<MDBBtn onClick={saveTime}>Save changes</MDBBtn>
+							</MDBModalFooter>
+						</MDBModalContent>
+					</MDBModalDialog>
+				</MDBModal>
+			</>
+		);
+	} else {
+		return (
+			<>
+				<MDBBtn color='tertiary' floating onClick={toggleModal}>
+					<MDBIcon icon='clock' />
+				</MDBBtn>
+				<MDBModal show={modalVis} setShow={setModalVis} tabIndex='-1'>
+					<MDBModalDialog>
+						<MDBModalContent>
+							<MDBModalHeader>
+								<MDBModalTitle>Reminder</MDBModalTitle>
+								<MDBBtn
+									className='btn-close'
+									color='none'
+									onClick={toggleModal}
+								></MDBBtn>
+							</MDBModalHeader>
+							<MDBModalBody>{secs}</MDBModalBody>
+
+							<MDBModalFooter>
+								<MDBBtn color='secondary' onClick={toggleModal}>
+									Close
+								</MDBBtn>
+								<MDBBtn onClick={toggleModal}>
+									Save changes
+								</MDBBtn>
 							</MDBModalFooter>
 						</MDBModalContent>
 					</MDBModalDialog>
